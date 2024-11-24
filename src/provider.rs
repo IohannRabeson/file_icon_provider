5acb5ede7f67d45ec26da62a86120d6716f2917d
IohannRabeson <IohannRabeson@users.noreply.github.com>
2024-11-24 18:25:41 -0500

    Add FileIconProvider

--- src/provider.rs
+++ src/provider.rs
@@ -1,35 +1,37 @@
-use std::{collections::{btree_map::Entry::{Occupied, Vacant}, BTreeMap}, ffi::OsStr, path::Path};
+use std::{
+    cell::RefCell,
+    collections::{
+        btree_map::Entry::{Occupied, Vacant},
+        BTreeMap,
+    },
+    ffi::OsString,
+    path::Path,
+};
 
 use crate::{get_file_icon, Icon};
 
-pub struct FileIconProvider<'a, T: Clone> {
-    cache: BTreeMap<&'a OsStr, T>,
+pub struct FileIconProvider<T: Clone> {
+    cache: RefCell<BTreeMap<OsString, T>>,
     convert: fn(Icon) -> T,
 }
 
-impl<'a, T: Clone> FileIconProvider<'a, T> {
+impl<T: Clone> FileIconProvider<T> {
     pub fn new(convert: fn(Icon) -> T) -> Self {
         Self {
-            cache: BTreeMap::new(),
+            cache: RefCell::new(BTreeMap::new()),
             convert,
         }
     }
 
-    pub fn icon(&mut self, path: &'a Path) -> Option<T> {
+    pub fn icon(&self, path: &Path) -> Option<T> {
         let get_icon = |path| get_file_icon(path).map(self.convert);
 
         match path.extension() {
-            Some(extension) => {
-                match self.cache.entry(extension) {
-                    Vacant(vacant_entry) => {
-                        Some(vacant_entry.insert(get_icon(path)?).clone())
-                    },
-                    Occupied(occupied_entry) => {
-                        Some(occupied_entry.get().clone())
-                    },
-                }
-            }
+            Some(extension) => match self.cache.borrow_mut().entry(extension.to_owned()) {
+                Vacant(vacant_entry) => Some(vacant_entry.insert(get_icon(path)?).clone()),
+                Occupied(occupied_entry) => Some(occupied_entry.get().clone()),
+            },
             None => get_icon(path),
-        }        
+        }
     }
 }
