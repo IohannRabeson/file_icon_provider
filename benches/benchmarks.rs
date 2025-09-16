36e6c7d1226c836c7456b169d0aeca614cfec66e
IohannRabeson <IohannRabeson@users.noreply.github.com>
2025-09-16 19:34:28 -0400

    Add a benchmark where we get the icon for a directory.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -19,5 +19,14 @@ fn provider_always_same_icon(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, always_same_icon, provider_always_same_icon);
+fn provider_directory_icon(c: &mut Criterion) {
+    let file_path = locate_cargo_manifest::locate_manifest().expect("locate Cargo.toml").parent().expect("parent folder of Cargo.toml").to_owned();
+    let provider = &file_icon_provider::Provider::new(32, Rc::new).unwrap();
+
+    c.bench_function("file_icon_provider::Provider::get_file_icon directory", |b| {
+        b.iter(|| provider.get_file_icon(black_box(&file_path)))
+    });
+}
+
+criterion_group!(benches, always_same_icon, provider_always_same_icon, provider_directory_icon);
 criterion_main!(benches);
