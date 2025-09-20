c56d05e406d91ceca13ce5fccc44a9a06c31a8d2
irabeson <iohannrabeson@users.noreply.github.com>
2025-09-20 18:05:01 -0400

    Log errors on MacOS.

--- src/implementation/macos.rs
+++ src/implementation/macos.rs
@@ -1,5 +1,7 @@
 use objc2::{AnyThread, rc::Retained};
-use objc2_app_kit::{NSBitmapImageRep, NSCompositingOperation, NSGraphicsContext, NSWorkspace};
+use objc2_app_kit::{
+    NSBitmapImageRep, NSCompositingOperation, NSGraphicsContext, NSImage, NSWorkspace,
+};
 use objc2_foundation::{NSPoint, NSRect, NSSize, NSString};
 use objc2_uniform_type_identifiers::UTType;
 
@@ -10,63 +12,19 @@ use std::{
     path::Path,
 };
 
+use log::error;
+
 pub(crate) fn get_file_icon(path: impl AsRef<Path>, size: u16) -> Option<Icon> {
-    let path = path.as_ref().canonicalize().ok()?;
-    let file_path = NSString::from_str(path.to_str()?);
-    let color_space_name = NSString::from_str("NSDeviceRGBColorSpace");
+    let file_path = path_to_nsstring(path)?;
     let shared_workspace = unsafe { NSWorkspace::sharedWorkspace() };
     let image = unsafe { shared_workspace.iconForFile(&file_path) };
-    let image_size = unsafe { image.size() };
-    let desired_size = NSSize {
-        width: size as f64,
-        height: size as f64,
-    };
-
-    if image_size.width < 1.0 || image_size.height < 1.0 {
-        return None;
-    }
-
-    let pixels = unsafe {
-        let bitmap_representation = NSBitmapImageRep::initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bytesPerRow_bitsPerPixel(
-                NSBitmapImageRep::alloc(),
-                std::ptr::null_mut(),
-                size as isize,
-                size as isize,
-                8,
-                4,
-                true,
-                false,
-                &color_space_name,
-                size as isize * 4,
-                32,
-            )?;
-        let context = NSGraphicsContext::graphicsContextWithBitmapImageRep(&bitmap_representation)?;
-
-        context.saveGraphicsState();
-
-        NSGraphicsContext::setCurrentContext(Some(&context));
-
-        image.setSize(desired_size);
-        image.drawAtPoint_fromRect_operation_fraction(
-            NSPoint::ZERO,
-            NSRect::new(NSPoint::ZERO, desired_size),
-            NSCompositingOperation::Copy,
-            1.0,
-        );
-        context.flushGraphics();
-        context.restoreGraphicsState();
-
-        std::slice::from_raw_parts(
-            bitmap_representation.bitmapData(),
-            bitmap_representation.bytesPerPlane() as usize,
-        )
-        .to_vec()
-    };
+    let bitmap_representation = create_bitmap_representation(size)?;
+    let context = create_context(&bitmap_representation)?;
 
     Some(Icon {
         width: size as u32,
         height: size as u32,
-        pixels,
+        pixels: get_pixels(&image, &context, &bitmap_representation, size as u32)?,
     })
 }
 
@@ -74,7 +32,6 @@ pub struct Provider<T: Clone> {
     shared_workspace: Retained<NSWorkspace>,
     bitmap_representation: Retained<NSBitmapImageRep>,
     context: Option<Retained<NSGraphicsContext>>,
-    desired_size: NSSize,
     icon_size: u32,
     cache: RefCell<BTreeMap<String, T>>,
     converter: fn(Icon) -> T,
@@ -85,37 +42,16 @@ where
     T: Clone,
 {
     pub fn new(icon_size: u16, converter: fn(Icon) -> T) -> Option<Self> {
-        let color_space_name = NSString::from_str("NSDeviceRGBColorSpace");
         let mut provider = Self {
             shared_workspace: unsafe { NSWorkspace::sharedWorkspace() },
-            bitmap_representation: unsafe {
-                NSBitmapImageRep::initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bytesPerRow_bitsPerPixel(
-                NSBitmapImageRep::alloc(),
-                std::ptr::null_mut(),
-                icon_size as isize,
-                icon_size as isize,
-                8,
-                4,
-                true,
-                false,
-                &color_space_name,
-                icon_size as isize * 4,
-                32,
-            )?
-            },
+            bitmap_representation: create_bitmap_representation(icon_size)?,
             context: None,
-            desired_size: NSSize {
-                width: icon_size as f64,
-                height: icon_size as f64,
-            },
             icon_size: icon_size as u32,
             cache: RefCell::new(BTreeMap::new()),
             converter,
         };
 
-        provider.context = Some(unsafe {
-            NSGraphicsContext::graphicsContextWithBitmapImageRep(&provider.bitmap_representation)?
-        });
+        provider.context = create_context(&provider.bitmap_representation);
 
         Some(provider)
     }
@@ -128,9 +64,7 @@ where
 
                     Some(vacant_entry.insert(icon).clone())
                 }
-                btree_map::Entry::Occupied(occupied_entry) => {
-                    Some(occupied_entry.get().clone())
-                }
+                btree_map::Entry::Occupied(occupied_entry) => Some(occupied_entry.get().clone()),
             },
             None => self.get_icon(path),
         }
@@ -149,35 +83,118 @@ where
 
     pub fn get_icon(&self, path: impl AsRef<Path>) -> Option<T> {
         let path = path.as_ref();
-
-        let pixels = unsafe {
-            let context = self.context.as_ref().unwrap();
-            let file_path = NSString::from_str(path.to_str()?);
-            let image = self.shared_workspace.iconForFile(&file_path);
-
-            context.saveGraphicsState();
-            NSGraphicsContext::setCurrentContext(Some(context));
-            image.setSize(self.desired_size);
-            image.drawAtPoint_fromRect_operation_fraction(
-                NSPoint::ZERO,
-                NSRect::new(NSPoint::ZERO, self.desired_size),
-                NSCompositingOperation::Copy,
-                1.0,
-            );
-            context.flushGraphics();
-            context.restoreGraphicsState();
-
-            std::slice::from_raw_parts(
-                self.bitmap_representation.bitmapData(),
-                self.bitmap_representation.bytesPerPlane() as usize,
-            )
-            .to_vec()
-        };
+        let context = self.context.as_ref().unwrap();
+        let file_path = path_to_nsstring(path)?;
+        let image = unsafe { self.shared_workspace.iconForFile(&file_path) };
 
         Some((self.converter)(Icon {
             width: self.icon_size,
             height: self.icon_size,
-            pixels,
+            pixels: get_pixels(&image, context, &self.bitmap_representation, self.icon_size)?,
         }))
+    }
+}
+
+fn create_bitmap_representation(icon_size: u16) -> Option<Retained<NSBitmapImageRep>> {
+    Some(
+        match unsafe {
+            let color_space_name = NSString::from_str("NSDeviceRGBColorSpace");
+
+            NSBitmapImageRep::initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bytesPerRow_bitsPerPixel(
+        NSBitmapImageRep::alloc(),
+        std::ptr::null_mut(),
+        icon_size as isize,
+        icon_size as isize,
+        8,
+        4,
+        true,
+        false,
+        &color_space_name,
+        icon_size as isize * 4,
+        32,
+    )
+        } {
+            Some(bitmap_representation) => bitmap_representation,
+            None => {
+                error!("Failed to create NSBitmapImageRep");
+                return None;
+            }
+        },
+    )
+}
+
+fn create_context(
+    bitmap_representation: &Retained<NSBitmapImageRep>,
+) -> Option<Retained<NSGraphicsContext>> {
+    Some(
+        match unsafe { NSGraphicsContext::graphicsContextWithBitmapImageRep(bitmap_representation) }
+        {
+            Some(context) => context,
+            None => {
+                error!("Failed to create graphics context");
+                return None;
+            }
+        },
+    )
+}
+
+fn get_pixels(
+    image: &NSImage,
+    context: &NSGraphicsContext,
+    bitmap_representation: &NSBitmapImageRep,
+    icon_size: u32,
+) -> Option<Vec<u8>> {
+    let image_size = unsafe { image.size() };
+
+    if image_size.width < 1.0 || image_size.height < 1.0 {
+        error!("Invalid image size");
+        return None;
+    }
+
+    let desired_size = NSSize {
+        width: icon_size as f64,
+        height: icon_size as f64,
+    };
+
+    Some(unsafe {
+        context.saveGraphicsState();
+        NSGraphicsContext::setCurrentContext(Some(context));
+        image.setSize(desired_size);
+        image.drawAtPoint_fromRect_operation_fraction(
+            NSPoint::ZERO,
+            NSRect::new(NSPoint::ZERO, desired_size),
+            NSCompositingOperation::Copy,
+            1.0,
+        );
+        context.flushGraphics();
+        context.restoreGraphicsState();
+
+        std::slice::from_raw_parts(
+            bitmap_representation.bitmapData(),
+            bitmap_representation.bytesPerPlane() as usize,
+        )
+        .to_vec()
+    })
+}
+
+fn path_to_nsstring(path: impl AsRef<Path>) -> Option<Retained<NSString>> {
+    let path = match path.as_ref().canonicalize() {
+        Ok(path) => path,
+        Err(error) => {
+            error!(
+                "Failed to canonicalize path '{}': {}",
+                path.as_ref().display(),
+                error
+            );
+            return None;
+        }
+    };
+
+    match path.to_str() {
+        Some(path) => Some(NSString::from_str(path)),
+        None => {
+            error!("Path '{}' is not valid unicode", path.display());
+            None
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -58,9 +58,9 @@ impl std::error::Error for Error {}
 ///     println!("Failed to retrieve the icon.");
 /// }
 /// ```
-/// 
-/// # Caveats 
-/// 
+///
+/// # Caveats
+///
 /// On linux, this function must be called on the main thread.
 pub fn get_file_icon(path: impl AsRef<Path>, size: u16) -> Result<Icon, Error> {
     // For consistency: on MacOS if the path does not exist None is returned
@@ -80,9 +80,9 @@ pub fn get_file_icon(path: impl AsRef<Path>, size: u16) -> Result<Icon, Error> {
 /// # Type Parameters
 /// * `T` - The type of the final image to be displayed. It must be clonable. If your image type can't be cloned
 ///   use a smart pointer (Rc).  
-/// 
+///
 /// Provider is interesting if you request a lot of icons with a fixed size.  
-/// It allocates internal buffers once and reuse them when the platform allows it (on MacOS). 
+/// It allocates internal buffers once and reuse them when the platform allows it (on MacOS).
 /// It caches icons reducing the CPU and memory usage.  
 pub struct Provider<T: Clone> {
     implementation: implementation::Provider<T>,
@@ -115,18 +115,18 @@ where
     /// ```
     /// use file_icon_provider::{Provider, Icon};
     /// use std::rc::Rc;
-    /// 
+    ///
     /// let provider: Provider<Rc<Icon>> = Provider::new(64, Rc::new).unwrap();
-    /// 
+    ///
     /// if let Ok(icon) = provider.get_file_icon("path/to/file") {
     ///     println!("Icon dimensions: {}x{}", icon.width, icon.height);
     /// } else {
     ///     println!("Failed to retrieve the icon.");
     /// }
     /// ```
-    /// 
-    /// # Caveats 
-    /// 
+    ///
+    /// # Caveats
+    ///
     /// On linux, this function must be called on the main thread.
     pub fn get_file_icon(&self, path: impl AsRef<Path>) -> Result<T, Error> {
         let path = path.as_ref();
