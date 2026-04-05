8b9d1f2c1d3296b1a91050386d04fe8d01a9caa9
irabeson <IohannRabeson@users.noreply.github.com>
2026-04-04 23:55:42 -0400

    Fix clippy warnings on Linux.

--- src/implementation/linux.rs
+++ src/implementation/linux.rs
@@ -14,7 +14,7 @@ pub(crate) fn get_file_icon(path: impl AsRef<Path>, size: u16) -> Option<Icon> {
 
     if !gtk::is_initialized() {
         if let Err(error) = gtk::init() {
-            error!("Failed to initialize GTK: {}", error);
+            error!("Failed to initialize GTK: {error}");
             return None;
         }
     }
@@ -23,43 +23,35 @@ pub(crate) fn get_file_icon(path: impl AsRef<Path>, size: u16) -> Option<Icon> {
     let file_info = match file.query_info("*", FileQueryInfoFlags::NONE, None::<&Cancellable>) {
         Ok(file_info) => file_info,
         Err(error) => {
-            error!("Can't get file info: {}", error);
+            error!("Can't get file info: {error}");
             return None;
         }
     };
-    let content_type = match file_info.content_type() {
-        Some(content_type) => content_type,
-        None => {
-            error!("Unable to get file content type");
-            return None;
-        }
+    let Some(content_type) = file_info.content_type() else {
+        error!("Unable to get file content type");
+        return None;
     };
     let icon = gio::functions::content_type_get_icon(&content_type);
-
-    let icon = match icon.dynamic_cast_ref::<gio::ThemedIcon>() {
-        Some(icon) => icon,
-        None => {
-            error!("Failed to cast icon into gio::ThemedIcon");
-            return None;
-        }
+    let Some(icon) = icon.dynamic_cast_ref::<gio::ThemedIcon>() else {
+        error!("Failed to cast icon into gio::ThemedIcon");
+        return None;
     };
-    let icon_theme = match IconTheme::default() {
-        Some(icon_theme) => icon_theme,
-        None => {
-            error!("Failed to create icon theme");
-            return None;
-        }
+    let Some(icon_theme) = IconTheme::default() else {
+        error!("Failed to create icon theme");
+        return None;
     };
 
     for name in icon.names() {
         if let Some(pixbuf) = icon_theme
-            .load_icon(&name, size as i32, IconLookupFlags::empty())
+            .load_icon(&name, i32::from(size), IconLookupFlags::empty())
             .ok()
             .flatten()
+        && let Ok(width) = u32::try_from(pixbuf.width())
+        && let Ok(height) = u32::try_from(pixbuf.height())
         {
             return Some(Icon {
-                width: pixbuf.width() as u32,
-                height: pixbuf.height() as u32,
+                width,
+                height,
                 pixels: pixbuf.read_pixel_bytes().to_vec(),
             });
         }
@@ -75,6 +67,7 @@ pub(crate) struct Provider<T: Clone> {
 }
 
 impl<T: Clone> Provider<T> {
+    #[allow(clippy::unnecessary_wraps)]
     pub fn new(icon_size: u16, converter: fn(Icon) -> T) -> Option<Self> {
         Some(Self {
             icon_size,
@@ -96,7 +89,7 @@ impl<T: Clone> Provider<T> {
         }
 
         match path.extension().and_then(OsStr::to_str) {
-            Some(".desktop") => get_file_icon(path, self.icon_size).map(self.converter),
+            Some(".desktop") | None => get_file_icon(path, self.icon_size).map(self.converter),
             Some(extension) => match self.icons_cache.borrow_mut().entry(extension.to_owned()) {
                 std::collections::btree_map::Entry::Vacant(vacant_entry) => Some(
                     vacant_entry
@@ -107,7 +100,6 @@ impl<T: Clone> Provider<T> {
                     Some(occupied_entry.get().clone())
                 }
             },
-            None => get_file_icon(path, self.icon_size).map(self.converter),
         }
     }
 }
